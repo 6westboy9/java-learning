d305f1755fc6c977909f74c7abe3f6fb03ae7bba
王鹏博 <wangpb@tanwuapp.com>
2021-01-22 18:50:59 +0800

    [新增]Netty相关

--- shengsiyuan-netty/src/main/java/com/westboy/demo06_protobuf_introduction/Demo06ProtobufStudentTest.java
+++ shengsiyuan-netty/src/main/java/com/westboy/demo06_protobuf_introduction/Demo06ProtobufStudentTest.java
@@ -1,11 +1,22 @@
 package com.westboy.demo06_protobuf_introduction;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
 /**
  * @author pengbo
  * @since 2021/1/20
  */
 public class Demo06ProtobufStudentTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        DataInfo.Student student1 = DataInfo.Student.newBuilder().setName("张三").setAge(20).setAddress("北京").build();
 
+        byte[] student2ByteArray = student1.toByteArray();
+
+        DataInfo.Student student2 = DataInfo.Student.parseFrom(student2ByteArray);
+        System.out.println(student2);
+        System.out.println("---");
+        System.out.println(student2.getName());
+        System.out.println(student2.getAge());
+        System.out.println(student2.getAddress());
     }
 }
---
+++ shengsiyuan-netty/src/main/java/com/westboy/demo07_protobuf_in_netty/server/TestServerInitializer.java
@@ -0,0 +1,35 @@
+package com.westboy.demo07_protobuf_in_netty.server;
+
+import com.westboy.demo07_protobuf_in_netty.MyDataInfo;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+/**
+ * @author westboy
+ * @date 2018-12-11
+ * @since 1.0
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new ProtobufVarint32FrameDecoder());
+        // 只能处理Student
+        pipeline.addLast(new ProtobufDecoder(MyDataInfo.MyMessage.getDefaultInstance()));
+        pipeline.addLast(new ProtobufVarint32LengthFieldPrepender());
+        pipeline.addLast(new ProtobufEncoder());
+        pipeline.addLast(new TestServerHandler());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
