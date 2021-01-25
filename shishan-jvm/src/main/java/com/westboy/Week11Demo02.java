8e660f4f782027ffe15673c382bdeb12b57ed8ef
王鹏博 <wangpb@tanwuapp.com>
2021-01-25 15:05:06 +0800

    [新增]JVM日志

---
+++ shishan-jvm/src/main/java/com/westboy/Week11Demo02.java
@@ -0,0 +1,26 @@
+package com.westboy;
+
+/**
+ * 手动模拟 Java 虚拟机栈 OOM
+ * <p>
+ * -XX:ThreadStackSize=1m
+ *
+ * <p>
+ * -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+ * -XX:ThreadStackSize=1m
+ * -XX:+PrintGCDetails -Xloggc:/Users/westboy/IdeaProjects/personal/java-learning/shishan-jvm-gc-log/week_11_demo_02.log
+ * -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/westboy/IdeaProjects/personal/java-learning/shishan-jvm-gc-log/
+ */
+public class Week11Demo02 {
+
+    public static long counter = 0;
+
+    public static void main(String[] args) {
+        work();
+    }
+
+    private static void work() {
+        System.out.println("目前是第" + (++counter) + "次调用方法");
+        work();
+    }
+}
